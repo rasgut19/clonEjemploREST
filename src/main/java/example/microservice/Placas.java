68691a1a2f5bb4e923217a76329aed1d2c1feedc
sala_c <sala_c@10.180.12.21>
2016-11-05 11:46:47 -0500

    Cambios WS Placa

--- src/main/java/example/microservice/Placas.java
+++ src/main/java/example/microservice/Placas.java
@@ -4,19 +4,13 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 
-@WebService(name="miServicio")
-public class micro {
+@WebService(name ="placas")
+public class Placas {
 
 	@WebMethod()
-	public String sayHello(@WebParam(name="nombre") String name) {
+	public String sayHello(String name) {
 	    System.out.println("Hello: " + name);
 	    return "Hello " + name + "!";
-	}
-	
-	@WebMethod()
-	public int sumar(@WebParam(name="valor1") int a,@WebParam(name="valor2") int b) {
-	    System.out.println("sumar: " + (a+b));
-	    return (a+b);
 	}
 	
 	@WebMethod()
